38638275debbd129b3d4bbaf3c2ebe81d2959ba0
obald12 <simugomwaobald250@gmail.com>
2025-05-01 22:13:40 +0200

    new_tests

--- student-registration-frontend/next.config.ts
+++ student-registration-frontend/next.config.ts
@@ -3,10 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   async rewrites() {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
     return [
       {
         source: "/api/:path*",
-        destination: process.env.NEXT_PUBLIC_API_URL + "/:path*",
+        destination: `${apiUrl}/:path*`,
       },
     ];
   },
